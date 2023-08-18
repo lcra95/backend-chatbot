2575e2f1fdc72b283692db7033d6c896fcbaf792
unknown <requenalc@bitbucket.org>
2023-08-18 16:07:08 -0400

    Elimino archivos innecesarios

--- aplicacion/app.py
+++ aplicacion/app.py
@@ -27,17 +27,7 @@ from aplicacion.redis import redis
 
 
 # IMPORTACIÓN DE RECURSOS
-from aplicacion.recursos.CASO import CasoResource
-from aplicacion.recursos.COMPAÑIA import CompañiaResource
-from aplicacion.recursos.CONTACTO import ContactoResource
-from aplicacion.recursos.ESTADO import EstadoResource
-from aplicacion.recursos.LOG import LogResource
-from aplicacion.recursos.MODULO import ModuloResource
-from aplicacion.recursos.PAI import PaiResource
-from aplicacion.recursos.PERFIL import PerfilResource
-from aplicacion.recursos.PERSONA import PersonaResource
-from aplicacion.recursos.PRODUCTO import ProductoResource
-from aplicacion.recursos.TIPOCASO import TipocasoResource
+
 
 #Inicializacion de flask
 app = Flask(__name__)
@@ -57,17 +47,7 @@ redis.init_app(app)
 api = Api(app)
 
 # SE DEFINEN LOS ENDPOINTS Y LA CLASE QUE SE ENCARGARÁ DE PROCESAR CADA SOLICITUD
-api.add_resource(CasoResource, '/caso/<int:_id>')
-api.add_resource(CompañiaResource, '/compañia/<int:_id>')
-api.add_resource(ContactoResource, '/contacto/<int:_id>')
-api.add_resource(EstadoResource, '/estado/<int:_id>')
-api.add_resource(LogResource, '/log/<int:_id>')
-api.add_resource(ModuloResource, '/modulo/<int:_id>')
-api.add_resource(PaiResource, '/pai/<int:_id>')
-api.add_resource(PerfilResource, '/perfil/<int:_id>')
-api.add_resource(PersonaResource, '/persona')
-api.add_resource(ProductoResource, '/producto/<int:_id>')
-api.add_resource(TipocasoResource, '/tipocaso/<int:_id>')
+
 
 #ROUTES
 @app.route('/')
@@ -75,6 +55,6 @@ def index():
     return "Hola =)", 200
 
 #INICIAMOS LA APLICACIÓN
-app.run(host='0.0.0.0', port=5000, debug=True )
+app.run(host='0.0.0.0', port=9090, debug=True )
     
         
